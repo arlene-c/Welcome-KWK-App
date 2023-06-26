a0df1999677f46c671d6ebd01b894f95acc47c58
arlene <arlene2k7@gmail.com>
2023-06-26 10:33:05 -0700

    Initial Commit

---
+++ Welcome KWK App/Welcome_KWK_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_KWK_AppApp.swift
+//  Welcome KWK App
+//
+//  Created by Arlene Chang on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_KWK_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
